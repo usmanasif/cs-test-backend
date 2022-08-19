9b728697d73292964118bcafa31e74b3a603ee0e
Usman Asif <pyroxicated@gmail.com>
2022-08-18 18:45:28 -0700

    Added Basic App Level Configurations

--- config/app.js
+++ config/app.js
@@ -1,28 +1,23 @@
-const expressConfig = require('./express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const expressConfig = require('./express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
 class AppConfig {
-    constructor(app) {
-        dotenv.config();
-        this.app = app;
-    }
-    includeConfig() {
-        this.app.use(cors())
-        this.app.use(bodyParser.json())
+  constructor(app) {
+    dotenv.config()
+    this.app = app
+  }
+  includeConfig() {
+    this.app.use(cors())
+    this.app.use(bodyParser.json())
 
-        this.app.use((req, res, next) => {
-            console.log("__________________________________");
-            console.log(`${new Date()} ${req.originalUrl}`);
-            console.log("Request Params: ", req.params);
-            console.log("Request Body: ", req.body);
-
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            return next();
-        })
-        new expressConfig(this.app);
-    }
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', req.headers.origin)
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      return next()
+    })
+    new expressConfig(this.app)
+  }
 }
-module.exports = AppConfig;
+module.exports = AppConfig
--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
 class ExpressConfig {
-    constructor(app) {
-        // Setting .html as the default template extension
-        app.set('view engine', 'html');
+  constructor(app) {
+    // Setting .html as the default template extension
+    app.set('view engine', 'html')
 
-        //Files 
-        app.use(require('express').static(require('path').join('public')));
-    }
+    //Files 
+    app.use(require('express').static(require('path').join('public')))
+  }
 }
-module.exports = ExpressConfig;
+module.exports = ExpressConfig
